d6822c8788a97f5b59be08c3478085761e28928e
olmoro <59123336+olmoro@users.noreply.github.com>
2025-01-21 17:16:32 +0300

    uart1

--- lib/staff/staff.c
+++ lib/staff/staff.c
@@ -3,16 +3,20 @@
  *    ESP32
  *====================================================================================*/
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "driver/uart.h"
+#include "freertos/queue.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
 
 #include "staff.h"
-#include <stdio.h>
-#include <stdint.h>
 #include "esp_err.h"
-#include "uart.h"
-//#include "mbcontroller.h"       // for mbcontroller defines and api
-//#include "modbus_params.h"      // for modbus parameters structures
-#include "esp_log.h"            // for log_write
-#include "sdkconfig.h"
 
 
 
@@ -23,7 +27,7 @@ static void staff_task(void *arg)
   const uart_port_t uart_num = UART_NUM_2;
   uart_config_t uart_config = 
   {
-      .baud_rate = 9600;    //115200,
+      .baud_rate = 9600,    //115200,
       .data_bits = UART_DATA_8_BITS,
       .parity = UART_PARITY_DISABLE,
       .stop_bits = UART_STOP_BITS_1,
@@ -34,8 +38,8 @@ static void staff_task(void *arg)
   // Configure UART parameters
   ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
   // Set UART pins(TX: IO4, RX: IO5, RTS: IO18, CTS: IO19)
-  // Set UART pins(TX: IO16, RX: IO17, RTS: IO19, CTS: no)
-  ESP_ERROR_CHECK(uart_set_pin(UART_NUM_2, 16, 17, 19, -1));
+  // Set UART pins(TX: IO27, RX: IO26, RTS: IO25, CTS: no)
+  ESP_ERROR_CHECK(uart_set_pin(UART_NUM_2, 27, 26, 25, -1));
 
   // Setup UART buffered IO with event queue
   const int uart_buffer_size = (1024 * 2);
