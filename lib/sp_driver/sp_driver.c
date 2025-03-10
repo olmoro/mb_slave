e810d6cbab591793cf7b688df57cf1fc7f44cf24
olmoro <urk2t@yandex.ru>
2025-03-10 20:52:36 +0300

    +драйвер

---
+++ lib/sp_driver/sp_driver.c
@@ -0,0 +1,93 @@
+/*
+ *
+ *
+*/
+
+#include "sp_driver.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "driver/uart.h"
+#include "freertos/queue.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+
+
+#define PACKET_READ_TICS        (100 / portTICK_PERIOD_MS)
+
+char rxSta;          // состояние процесса приема пакета
+char rxPre;          // предыдущий принятый байт
+char rxAdd;          // адрес, с которым сравнивается принятый
+char rxCmd;          // принятая команда
+char rxCrc;          // контрольная сумма принимаемого пакета
+unsigned char rxPtr; // указатель на массив принимаемых данных
+
+char txSta;          // состояние процесса передачи пакета
+char txPre;          // предыдущий переданный байт
+char txAdd;          // адрес, передававемый в пакете
+char txCrc;          // контрольная сумма передаваемого пакета
+unsigned char txPtr; // указатель на массив передаваемых данных
+
+// Переменные - уточнить типы  
+char    rxNbt;          // принятое количество байт в пакете
+char    rxDat[FRAME];   // массив принятых данных
+uint8_t command;        // код команды на выполнение
+
+char    txCmd;          // команда, передаваемая в пакете
+uint8_t txNbt;          // количество байт данных в пакете
+char    txDat[FRAME];   // массив данных для передачи
+
+uart_port_t port_sp;
+
+// void wakeInit( uint8_t addr, long time )
+// {
+//   //const int uart_num = CONFIG_SP_UART_PORT_NUM;
+//   port_sp = CONFIG_SP_UART_PORT_NUM;
+//   // #ifdef UART2
+//   //   //Serial2.begin(115200);            // это порт дрйвера
+//   //   Serial2.begin(230400);            // это порт дрйвера - май 2022
+//   //   Serial2.setTimeout( time );       // время тайм-аута для функции readBytes(), ms. 
+//   // #endif
+
+//   rxAdd   = addr;                     // адрес на прием
+//   txAdd   = addr;                     // адрес на передачу
+//   rxSta   = WAIT_FEND;                // ожидание пакета
+//   txSta   = SEND_IDLE;                // ничего пока не передаем
+//   command = CMD_NOP;                  // нет команды на выполнение
+// }
+
+// // Вычисление контрольной суммы
+// static void doCrc8(char b, char *crc)
+// {
+//   char i;
+//   for(i = 0; i < 8; b = b >> 1, i++)
+//     if((b ^ *crc) & 1) *crc = ((*crc ^ 0x18) >> 1) | 0x80;
+//     else *crc = (*crc >> 1) & ~0x80;
+// }
+
+/* Функция вычисляет и возвращает циклический код для 
+  последовательности из len байтов, указанной *msg.
+  Используется порождающий полином:
+  (X в степени 16)+(X в степени 12)+(X в степени 5)+1.
+  Полиному соответствует битовая маска 0x1021.
+*/
+int CRCode(char *msg, int len)
+{
+    int j, crc = 0;
+    while (len-- > 0)
+    {
+        crc = crc ^ (int)*msg++ << 8;
+        for (j = 0; j < 8; j++)
+        {
+            if (crc & 0x8000)
+                crc = (crc << 1) ^ 0x1021;
+            else
+                crc <<= 1;
+        }
+    }
+    return crc;
+}
---
+++ lib/sp_driver/sp_driver.h
@@ -0,0 +1,72 @@
+/*
+
+*/
+#ifndef _SP_DRIVER_H_
+#define _SP_DRIVER_H_
+
+#include <stdint.h>
+
+// Константы протокола
+#define SOH 0x01        // Байт начала заголовка
+#define ISI 0x1F        // Байт указателя кода функции
+#define STX 0x02        // Байт начала тела сообщения
+#define ETX 0x03        // Байт конца тела сообщения
+#define DLE 0x10        // Байт символа-префикса
+#define CRC_INIT 0x1021 // Битовая маска полинома
+#define FRAME 128       //
+
+// RX process states:
+enum
+{
+    WAIT_SOH = 0,   // ожидание приема заголовка
+    WAIT_DAD,       // ожидание приема адреса приёмника
+    WAIT_SAD,       // ожидание приема адреса источника
+    WAIT_FNC,       // ожидание приема кода функции
+    WAIT_DATA_HEAD, // прием данных заголовка
+    WAIT_DATA_SET,  // прием данных сообщения
+    WAIT_CRC,       // ожидание окончания приема CRC
+    WAIT_CARR       // ожидание несущей
+};
+
+// TX process states: (не корректировано)
+enum
+{
+    SEND_IDLE, // состояние бездействия
+    SEND_ADDR, // передача адреса
+    SEND_CMD,  // передача команды
+    SEND_NBT,  // передача количества байт в пакете
+    SEND_DATA, // передача данных
+    SEND_CRC,  // передача CRC
+    SEND_END   // окончание передачи пакета
+};
+
+// Коды универсальных команд:
+
+// Коды ошибок:
+
+void wakeRead();
+
+//     // передача ответа на команду
+//     void txReplay(char n, char err);
+void configAsk(char n, uint8_t comm); // для активного контроллера - без байта подтверждения
+
+// работа с буфером приема
+uint8_t getCommand();
+uint8_t get08(int i);
+uint16_t get16(int i);
+float getF16(int i);
+uint8_t getNbt(); // получить число байт в ответе
+
+// работа с буфером передачи
+void setU8(int id, uint8_t value);
+
+int replyU08(int id, uint8_t value);
+int replyU16(int id, uint16_t value);
+int replyU32(int id, uint32_t value);
+
+void testReply(int n); // тест отправить n байт из буфера приемника
+
+void wakeWrite();
+void wakeStartWrite();
+
+#endif // _SP_DRIVER_H_
